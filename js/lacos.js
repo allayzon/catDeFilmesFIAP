3894c4f13dabe6acb4f9718ca288793fe0c6ca4e
allayzon <allyson.g.d.santos@gmail.com>
2022-04-28 11:23:01 -0300

    Estudando incremento de valores em variáveis

---
+++ js/lacos.js
@@ -0,0 +1,13 @@
+//! Laços de repetição são: For, While, Do ... While
+
+//* Vamos construir um loop para fazer as tabuadas
+
+// let a = 0
+// console.log(a = 0 + 1) // a = 1
+// console.log(a = a + 1) // a = 1 + 1 = 2
+// console.log(a += 1) // a = 2 + 1 = 3
+// console.log(++a); // a = 3 + 1 = 4
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
