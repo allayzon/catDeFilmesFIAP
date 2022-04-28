0ca4f11818a7f872082b02f43d77e60065690134
allayzon <allyson.g.d.santos@gmail.com>
2022-04-28 11:42:50 -0300

    Construindo um laço de repetição for que mostra a tabuada de cada número 1 - 10

--- js/lacos.js
+++ js/lacos.js
@@ -9,5 +9,8 @@
 // console.log(++a); // a = 3 + 1 = 4
 
 for (let i = 0; i <= 10; i++) {
-    console.log(i);
+    console.log(`A tabuada do ${i} é:`);
+    for(let j = 0; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
 }
