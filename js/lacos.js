544323ea7dbfc7a8f69c6cf7a6ff035ce500af13
allayzon <allyson.g.d.santos@gmail.com>
2022-04-28 13:43:47 -0300

    Comparando laço while com do while

--- js/lacos.js
+++ js/lacos.js
@@ -8,9 +8,23 @@
 // console.log(a += 1) // a = 2 + 1 = 3
 // console.log(++a); // a = 3 + 1 = 4
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`A tabuada do ${i} é:`);
-    for(let j = 0; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`A tabuada do ${i} é:`);
+//     for(let j = 0; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
+
+//* Construindo um loop while
+
+let a = 0
+
+while (a < 3) {
+    console.log('Bom dia do laço while!');
+    a++
 }
+
+do {
+    console.log('Bom dia do laço do ... while!');
+    a++;
+} while (a > 0)
