539c23bbae911b9c18ff50f3280b00619ad4cc01
allayzon <allyson.g.d.santos@gmail.com>
2022-04-28 15:11:31 -0300

    Adicionando os links dos filmes e os títulos em seus respectivos arrays

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-function cadastrar() {
-    console.log('Fui cadastrado com sucesso!')
-}
+let listaDeFilmes = [
+    'https://br.web.img3.acsta.net/pictures/14/08/11/13/34/537036.jpg',
+    'https://img.elo7.com.br/product/original/39B7AE4/quadro-interestelar-a3-com-vidro-quadro-maritimo.jpg',
+    'https://br.web.img3.acsta.net/pictures/210/140/21014024_20130619225537406.jpg',
+    'https://ingresso-a.akamaihd.net/img/cinema/cartaz/6429-cartaz.jpg',
+    'https://br.web.img2.acsta.net/medias/nmedia/18/87/89/23/19962617.jpg',
+    'https://seuladogeek.com.br/wp-content/uploads/2022/04/Jogador-No-1.jpg'
+]
+
+let listaDeTitulos = [
+    'Relatos Selvagens',
+    'Interestelar',
+    'Truque de Mestre',
+    'Cantando na Chuva',
+    'Efeito Borboleta',
+    'Jogador Nº 1'
+]
